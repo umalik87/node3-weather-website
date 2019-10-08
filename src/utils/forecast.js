7a44d6e7b7fe45a2eade9d5c8d779db0d948b501
umalik87 <umalik87@gmail.com>
2019-10-07 22:52:39 -0400

    Update forecast daily high and daily low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find the location.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ` It is currently ${body.currently.temperature} degrees out. There is ${body.currently.precipProbability} chance of rain.`
+            callback(undefined, body.daily.data[0].summary + ` It is currently ${body.currently.temperature} degrees out. There is ${body.currently.precipProbability} chance of rain. 
+            The high today is ${body.daily.data[0].temperatureHigh}, and the low is ${body.daily.data[0].temperatureLow}. `
                 )
         }
     } )
